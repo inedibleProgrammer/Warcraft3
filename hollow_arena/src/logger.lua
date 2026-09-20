732543b50bb246989d28cf59451b62a6391ff257
inedible programmer <inedibleprogrammer@gmail.com>
2026-09-20 07:14:47 -0400

    Add logger improvements

--- hollow_arena/src/logger.lua
+++ hollow_arena/src/logger.lua
@@ -1,5 +1,11 @@
 local Logger = {}
 
+Logger.DEBUG    = "DEBUG"
+Logger.INFO     = "INFO"
+Logger.WARNING  = "WARNING"
+Logger.FAILURE  = "FAILURE"
+Logger.CRITICAL = "CRITICAL"
+
 -- name is a string
 -- get_time is a function returning number of seconds
 function Logger.new(name, get_time)
@@ -11,12 +17,31 @@ function Logger.new(name, get_time)
   -- levels: DEBUG < INFO < WARNING < FAILURE < CRITICAL
   function logger:log(level, message)
     local entry = {
+      index = 1 + #self.entries,
       time = get_time(),
       level = level,
       message = message,
     }
 
     table.insert(self.entries, entry)
+  end
+
+  function logger:format(entry)
+    local formatted_message =
+      "[" .. tostring(entry.index) .. "] " ..
+      "[" .. tostring(entry.time) .. "] " ..
+      "[" .. entry.level .. "] " ..
+      entry.message
+    return formatted_message
+  end
+
+  -- Pre-bound callback suitable for xpcall.
+  logger.error_handler = function(err)
+    local message = tostring(err)
+
+    logger:log(Logger.FAILURE, message)
+
+    return message
   end
 
   return logger
--- hollow_arena/test/unit/test_logger.lua
+++ hollow_arena/test/unit/test_logger.lua
@@ -3,6 +3,7 @@
 
 local lu = require("luaunit")
 local logger = require("logger")
+-- local dbg = require("debugger")
 
 TestLogger = {}
 TestLogger.fake_time = 0
@@ -20,11 +21,18 @@ function TestLogger.test_logger_counter()
   local testlogger = logger.new("logger", fake_time_recorder)
 
   TestLogger.fake_time = 10
-  testlogger:log("INFO", "first info message")
+  testlogger:log(logger.INFO, "first info message")
 
+  lu.assertEquals(testlogger.entries[1].index, 1)
   lu.assertEquals(testlogger.entries[1].time, 10)
   lu.assertEquals(testlogger.entries[1].level, "INFO")
   lu.assertEquals(testlogger.entries[1].message, "first info message")
   lu.assertEquals(#testlogger.entries, 1)
+
+  testlogger:log(logger.INFO, "second info message")
+  lu.assertEquals(testlogger.entries[2].index, 2)
+  lu.assertEquals(#testlogger.entries, 2)
+
+  lu.assertEquals(testlogger:format(testlogger.entries[1]), "[1] [10] [INFO] first info message")
 end
 
